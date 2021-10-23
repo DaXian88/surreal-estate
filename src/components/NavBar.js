ae4a96d3e7684360cfc1e853b3f2bd540f3d2a41
Khalid Safy <khalid_safy88@hotmail.com>
2021-10-23 10:49:37 +0100

    Refactored NavBar component to route links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/NavBar.css";
 
 const NavBar = () => {
@@ -9,8 +10,12 @@ const NavBar = () => {
         alt="Logo"
       ></img>
       <ul className="navbar-links">
-        <li className="navbar-links-item">View Properties</li>
-        <li className="navbar-links-item">Add a Property</li>
+        <li className="navbar-links-item">
+          <Link to="/">View Properties</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/add-property">Add a Property</Link>
+        </li>
       </ul>
     </div>
   );
