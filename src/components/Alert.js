1cadcb644bb02d435673000cc54e8d6d0dbf5082
Khalid Safy <khalid_safy88@hotmail.com>
2021-11-01 23:46:15 +0000

    Added proptype validation to Alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,16 @@
+import PropTypes from "prop-types";
+
 const Alert = ({ message, success }) => {
   return (
     <div className={`Alert alert-${success ? "success" : "error"}`}>
       {message}
     </div>
   );
+};
+
+Alert.propTypes = {
+  message: PropTypes.string.isRequired,
+  success: PropTypes.bool,
 };
 
 export default Alert;
