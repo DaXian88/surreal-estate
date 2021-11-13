a8b5559fc6591065514de52d51fbef33b93a0da9
Khalid Safy <khalid_safy88@hotmail.com>
2021-11-13 23:51:26 +0000

    Passed PropertyCard to Properties component, rendering with dummy props

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,5 +1,20 @@
+import PropertyCard from "./PropertyCard";
+
 const Properties = () => {
-  return <div>Properties Page</div>;
+  return (
+    <div>
+      Properties Page
+      <PropertyCard
+        title="Beautiful 3 Bed"
+        type="Semi-Detached"
+        city="Manchester"
+        bathrooms="1"
+        bedrooms="3"
+        price="100,000"
+        email="kal@123.com"
+      />
+    </div>
+  );
 };
 
 export default Properties;
