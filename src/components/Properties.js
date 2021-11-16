72d006d44bde445ecb5cec91d437f2853f368a78
Khalid Safy <khalid_safy88@hotmail.com>
2021-11-16 22:55:50 +0000

    Mapped over properties state to pass key/value pairs to PropertyCard component

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -17,15 +17,9 @@ const Properties = () => {
   return (
     <div>
       Properties Page
-      <PropertyCard
-        title="Beautiful 3 Bed"
-        type="Semi-Detached"
-        city="Manchester"
-        bathrooms="1"
-        bedrooms="3"
-        price="100,000"
-        email="kal@123.com"
-      />
+      {properties.map((property) => (
+        <PropertyCard key={property._id} {...property} />
+      ))}
     </div>
   );
 };
